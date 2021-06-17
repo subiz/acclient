e087d863eef3a89ab940824281bf1f93ba9e59c6
Thanh Pham Kieu <thanhpk@live.com>
2021-06-17 17:30:13 +0700

    add metrics

---
+++ metrics.go
@@ -0,0 +1,47 @@
+package acclient
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"sync"
+	"time"
+)
+
+var metricmaplock = &sync.Mutex{}
+var metricmap = make(map[string]float64)
+
+func init() {
+	go flush()
+}
+
+func flush() {
+	// flush periodically in 30s
+	for {
+		start := time.Now()
+		metricmaplock.Lock()
+		metricmapcopy := make(map[string]float64)
+		for k, v := range metricmap {
+			metricmapcopy[k] = v
+		}
+		metricmap = make(map[string]float64)
+		metricmaplock.Unlock()
+
+		for k, v := range metricmapcopy {
+			resp, err := http.Post("http://metric-0.metric/gauges/"+k+"/collects/"+strconv.FormatFloat(v, 'E', -1, 32), "", nil)
+			if err != nil || resp.StatusCode != 200 {
+				fmt.Println("METRIC ERR", err.Error())
+			}
+		}
+
+		fmt.Println("METRIC FLUSHING DONE:", len(metricmap), "in", time.Since(start))
+		time.Sleep(3 * time.Second)
+	}
+}
+
+// min
+func CollectGause(metric string, value float64) {
+	metricmaplock.Lock()
+	metricmap[metric] = metricmap[metric] + value
+	metricmaplock.Unlock()
+}
