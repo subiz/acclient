782f158c95fd778bea15873657d74080cc95d6d2
Thanh Pham Kieu <thanhpk@live.com>
2021-07-12 00:29:26 +0700

    fix list agent

--- main.go
+++ main.go
@@ -396,16 +396,16 @@ func listAgentsDB(accid string) ([]*pb.Agent, error) {
 	var arr = make([]*pb.Agent, 0)
 
 	var id, avatar_url, client_id, country_code, email, encrypted_password, fullname, gender string
-	var invited_by, jobtitle, lang, phone string
+	var invited_by, jobtitle, lang, phone, state, typ, tz string
 	var isowner, issupervisor bool
 	var emails, phones, scopes []string
-	var joined, lasttokenrequested, passwordchanged int64
+	var joined, lasttokenrequested, passwordchanged, seen int64
 	var dashboard_setting []byte
 	var modified int64
 
-	iter := session.Query("SELECT id, avatar_url, client_id, country_code, dashboard_setting, email, emails, encrypted_password, fullname, gender, invited_by, is_owner, is_supervisor, job_title, joined, lang, last_token_requested, modified, password_changed, phone, phones, scopes FROM account.agents where account_id=?", accid).Iter()
+	iter := session.Query("SELECT id, avatar_url, client_id, country_code, dashboard_setting, email, emails, encrypted_password, fullname, gender, invited_by, is_owner, is_supervisor, job_title, joined, lang, last_token_requested, modified, password_changed, phone, phones, scopes, state, type, timezone, seen FROM account.agents where account_id=?", accid).Iter()
 	for iter.Scan(&id, &avatar_url, &client_id, &country_code, &dashboard_setting, &email, &emails, &encrypted_password, &fullname, &gender, &invited_by,
-		&isowner, &issupervisor, &jobtitle, &joined, &lang, &lasttokenrequested, &modified, &passwordchanged, &phone, &phones, &scopes) {
+		&isowner, &issupervisor, &jobtitle, &joined, &lang, &lasttokenrequested, &modified, &passwordchanged, &phone, &phones, &scopes, &state, &typ, &tz, &seen) {
 		ds := &pb.DashboardAgent{}
 		proto.Unmarshal(dashboard_setting, ds)
 		ag := &pb.Agent{
@@ -432,6 +432,10 @@ func listAgentsDB(accid string) ([]*pb.Agent, error) {
 			Phone:              conv.S(phone),
 			Phones:             phones,
 			Scopes:             scopes,
+			State:              conv.S(state),
+			Type:               conv.S(typ),
+			Timezone:           conv.S(tz),
+			Seen:               conv.PI64(int(seen)),
 		}
 		emails = make([]string, 0)
 		phones = make([]string, 0)
