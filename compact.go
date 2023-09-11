7b14520e1e0f11c8443a5deaed5a6188b1fdaa2a
thienluc2017 <lethienluc2015@gmail.com>
2023-09-11 13:55:48 +0700

    add version2 for uncompact and compact

--- compact.go
+++ compact.go
@@ -12,13 +12,17 @@ var (
 
 	compactCache, _   = lru.New[string, int](10_000)
 	uncompactCache, _ = lru.New[int, string](10_000)
+
+	compactCache2, _   = lru.New[string, int](10_000)
+	uncompactCache2, _ = lru.New[int, string](10_000)
 )
 
-func CompactString(str string) (int, error) {
+func CompactString(str string, version string) (int, error) {
 	if str == "" {
 		return 0, nil
 	}
 	waitUntilReady()
+
 	number, exist := compactCache.Get(str)
 	if exist {
 		return number, nil
@@ -41,12 +45,38 @@ func CompactString(str string) (int, error) {
 	return number, nil
 }
 
-func UncompactString(num int) (string, error) {
+func CompactString2(str string) (int, error) {
+	if str == "" {
+		return 0, nil
+	}
+	waitUntilReady()
+	number, exist := compactCache2.Get(str)
+	if exist {
+		return number, nil
+	}
+
+	err := session.Query(`SELECT num FROM account.compact_str2 WHERE str=?`, str).Scan(&number)
+	if err == nil {
+		uncompactCache2.Add(number, str)
+		compactCache2.Add(str, number)
+		return number, nil
+	}
+
+	numOut, err := registryClient.Compact(context.Background(), &header.String{Str: str, Version: "2"})
+	if err != nil {
+		return 0, err
+	}
+	number = int(numOut.GetNumber())
+	uncompactCache2.Add(number, str)
+	compactCache2.Add(str, number)
+	return number, nil
+}
+
+func UncompactString(num int, version string) (string, error) {
 	if num == 0 {
 		return "", nil
 	}
 	waitUntilReady()
-
 	str, exist := uncompactCache.Get(num)
 	if exist {
 		return str, nil
@@ -66,5 +96,32 @@ func UncompactString(num int) (string, error) {
 	str = strOut.GetStr()
 	uncompactCache.Add(num, str)
 	compactCache.Add(str, num)
+	return str, nil
+}
+
+func UncompactString2(num int) (string, error) {
+	if num == 0 {
+		return "", nil
+	}
+	waitUntilReady()
+	str, exist := uncompactCache2.Get(num)
+	if exist {
+		return str, nil
+	}
+
+	err := session.Query(`SELECT str FROM account.uncompact_num2 WHERE num=?`, num).Scan(&str)
+	if err == nil {
+		uncompactCache2.Add(num, str)
+		compactCache2.Add(str, num)
+		return str, nil
+	}
+
+	strOut, err := registryClient.Uncompact(context.Background(), &header.Number{Number: int64(num), Version: "2"})
+	if err != nil {
+		return "", err
+	}
+	str = strOut.GetStr()
+	uncompactCache2.Add(num, str)
+	compactCache2.Add(str, num)
 	return str, nil
 }
